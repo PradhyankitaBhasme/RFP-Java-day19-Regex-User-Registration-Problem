b93f69f0b7e7da434cdc7d82406a93f7737b75e4
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-01 22:12:06 +0530

    validate first name

---
+++ src/com/bridgelabz/Regex.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+import javax.management.DynamicMBean;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Regex {
+    public static void main(String[] args) {
+        Regex regex=new Regex();
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter first name :");
+        String firstName=scanner.next();
+        regex.validateFirstName(firstName);
+
+    }
+
+    public void validateFirstName(String firstName){
+//        first name starts with Cap and has minimum 3 characters
+        Pattern pattern=Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+        boolean matches = pattern.matcher(firstName).matches();
+        if (matches){
+            System.out.println("firstName is valid");
+        }else
+            System.out.println("firstName is not valid");
+    }
+}
