a162550baba73442fdc9e6ecb58c913cb17f95e9
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-01 23:12:04 +0530

    validate email address

--- src/com/bridgelabz/Regex.java
+++ src/com/bridgelabz/Regex.java
@@ -15,6 +15,10 @@ public class Regex {
         System.out.println("Enter last name: ");
         String lastName=scanner.next();
         regex.validateName(lastName);
+
+        System.out.println("Enter email: ");
+        String email=scanner.next();
+        regex.validateEmail(email);
     }
 
     public void validateName(String name){
@@ -25,5 +29,17 @@ public class Regex {
             System.out.println(name+" is valid");
         }else
             System.out.println(name+" is not valid");
+    }
+    public void validateEmail(String email){
+/*        E.g. abc.xyz@bl.co.in
+        Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in)
+        with precise @ and . positions
+ */
+        Pattern pattern=Pattern.compile("^[a-zA-Z0-9]+ ([._-][a-zA-Z0-9]+)* @ [a-z]+ ([.][a-z])* ([.][a-z]+)$");
+        boolean matches = pattern.matcher(email).matches();
+        if (matches){
+            System.out.println("email is valid");
+        }else
+            System.out.println("email is not valid");
     }
 }
