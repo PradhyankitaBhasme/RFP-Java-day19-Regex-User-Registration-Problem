a09b31399c823f65af726b3f16952ddf1bb89536
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-04 14:17:52 +0530

    Added JUnit Email Parameterised Tests

--- src/test/java/com/junit/UserRegistrationTest.java
+++ src/test/java/com/junit/UserRegistrationTest.java
@@ -4,6 +4,8 @@ import com.bridgelabz.UserRegistrationRegex;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserRegistrationTest {
     static UserRegistrationRegex userRegistration;
@@ -70,5 +72,17 @@ public class UserRegistrationTest {
     public void givenPassword_WhenInValid_ShouldReturnFalse(){
         boolean result=userRegistration.validatePassword("96526d");
         Assertions.assertFalse(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com.com","abc+100@gmail.com"})
+    public void givenEmail_WhenValid_ShouldReturnTrue(String email) {
+        Assertions.assertTrue(userRegistration.validateEmail(email));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"})
+    public void givenEmail_WhenInValid_ShouldReturnFalse(String email) {
+        Assertions.assertFalse(userRegistration.validateEmail(email));
     }
 }
