5a3cb279f8e87f01e5f8e94b1af0185e91c46ba9
sagarchavan07 <chavansagar96044@gmail.com>
2022-08-05 12:53:17 +0530

    Refctor code throw custom Exception InvalidUserDetailsException

--- src/main/java/com/bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -3,41 +3,27 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class UserRegistrationMain {
-    public static void main(String[] args) {
-        UserRegistrationRegex regex=new UserRegistrationRegex();
-        Scanner scanner=new Scanner(System.in);
+    public static void main(String[] args) throws InvalidUserDetailsException {
+        UserRegistrationRegex regex = new UserRegistrationRegex();
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter first name: ");
-        String firstName=scanner.next();
-        regex.validateFirstName(firstName);
+        regex.validateFirstName(scanner.next());
 
         System.out.println("\nEnter last name: ");
-        String lastName=scanner.next();
-        regex.validateLastName(lastName);
+        regex.validateLastName(scanner.next());
 
         System.out.println("\nEnter email: ");
-        String email=scanner.next();
-        regex.validateEmail(email);
+        regex.validateEmail(scanner.next());
 
         System.out.println("\nEnter mobile no as per format: (e.g. 91 9867859848) ");
         scanner.nextLine();
-        String mobileNo=scanner.nextLine();
-        regex.validateMobileNo(mobileNo);
+        regex.validateMobileNo(scanner.nextLine());
 
         System.out.println("\nEnter Password:");
-        String password=scanner.next();
-        regex.validatePassword(password);
+        regex.validatePassword(scanner.next());
 
         System.out.println("\nValid Emails: ");
-        String[] validEmail={"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com.com","abc+100@gmail.com"};
-        for (String s : validEmail) {
-            regex.validateEmail(s);
-        }
-
-        System.out.println("\nInValid Emails: ");
-        String[] inValidEmail={"abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"};
-        for (String s : inValidEmail) {
-            regex.validateEmail(s);
-        }
+        regex.validateEmail(scanner.next());
     }
 }
